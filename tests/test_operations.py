80d528b23ad07be71d7a92386dd21ebf59eed95d
Samay  Jain <samay_j@me.iitr.ac.in>
2026-08-01 18:24:28 +0530

    Unit Test cases Updated

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from src.math_operations import add,sub 
+
+def test_add():
+    assert add(2,3)==5 
+    assert add(-1,1)==0
+   
+
+
+def test_sub():
+     assert sub(5,3)==2
+     assert sub(5,1)==4
+     assert sub(6,1)==5
+
